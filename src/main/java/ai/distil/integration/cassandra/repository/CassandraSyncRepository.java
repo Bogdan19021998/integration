deec06c7a6e4ea6a4dced7d9af1cb128c72ceb64
Neil <neil@big-consultancy.com>
2019-07-15 09:30:11 +0100

    Change the default new org keyspace name

--- src/main/java/ai/distil/integration/cassandra/repository/CassandraSyncRepository.java
+++ src/main/java/ai/distil/integration/cassandra/repository/CassandraSyncRepository.java
@@ -53,7 +53,7 @@ public class CassandraSyncRepository {
     private static final String HASH_COLUMN = "h";
     private static final String CREATED_AT_COLUMN = "c";
     private static final String UPDATED_AT_COLUMN = "u";
-    private static final String KEYSPACE_PREFIX = "ORG_";
+    private static final String KEYSPACE_PREFIX = "distil_org_";
 
     private final CassandraConnection connection;
 
