26b07557a159432179d009eca70577033fc3dd4d
Vitaliy Samofal <vsamofal93@gmail.com>
2019-07-30 21:24:05 +0300

    Fixed ssh connection

--- src/main/java/ai/distil/integration/ssh/ConnectionParamsFactory.java
+++ src/main/java/ai/distil/integration/ssh/ConnectionParamsFactory.java
@@ -6,13 +6,13 @@ public class ConnectionParamsFactory {
 
     public static SshConnectionParameters buildConnectionParams(ConnectionSettings connSettings) {
         if (connSettings.isSsh_enabled()) {
-            if (connSettings.getSsh_password() != null) {
-                return new PasswordSshConnectionParameters(connSettings.getServerAddress(), connSettings.getSsh_port(),
+            if (connSettings.getSsh_key() != null) {
+                return new KeySshConnectionParameters(connSettings.getSsh_serverAddress(), connSettings.getSsh_port(),
+                        connSettings.getSsh_userName(), connSettings.getSsh_passphrase(), connSettings.getSsh_key());
+            } else if (connSettings.getSsh_password() != null) {
+                return new PasswordSshConnectionParameters(connSettings.getSsh_serverAddress(), connSettings.getSsh_port(),
                         connSettings.getSsh_userName(),
                         connSettings.getSsh_password());
-            } else if (connSettings.getSsh_key() != null) {
-                return new KeySshConnectionParameters(connSettings.getServerAddress(), connSettings.getSsh_port(),
-                        connSettings.getSsh_userName(), connSettings.getSsh_passphrase(), connSettings.getSsh_key());
             } else {
                 return null;
             }
