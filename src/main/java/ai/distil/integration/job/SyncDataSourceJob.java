a7094f2b346afd34fcb8c786bc13347b6a78968f
Vitaliy Samofal <vsamofal93@gmail.com>
2019-07-30 16:52:08 +0300

    Added source attributes field, for avoid filtering by verified status

--- src/main/java/ai/distil/integration/job/SyncDataSourceJob.java
+++ src/main/java/ai/distil/integration/job/SyncDataSourceJob.java
@@ -80,7 +80,7 @@ public class SyncDataSourceJob extends QuartzJobBean {
             if (connection.dataSourceExist(dataSource)) {
                 DataSourceDataHolder updatedSchema = dataSyncService.syncSchema(request.getTenantId(), dataSource, connection);
 
-                connectionProxy.updateDataSourceData(request.getTenantId(), request.getDataSourceId(), new UpdateDataSourceDataRequest(null, updatedSchema.getAllAttributes()));
+                connectionProxy.updateDataSourceData(request.getTenantId(), request.getDataSourceId(), new UpdateDataSourceDataRequest(null, updatedSchema.getSourceAttributes()));
                 dataSyncService.syncDataSource(request.getTenantId(), updatedSchema, connection);
             } else {
                 log.info("The data source no longer exists?? - updating the datasource details");
