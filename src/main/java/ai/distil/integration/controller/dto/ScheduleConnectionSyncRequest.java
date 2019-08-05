4e014e728cea9aeacd26429ec9bbdc36cdaa9b76
Vitaliy Samofal <vsamofal93@gmail.com>
2019-08-05 18:59:47 +0300

    Removed all args constructor for schedule request

--- src/main/java/ai/distil/integration/controller/dto/ScheduleConnectionSyncRequest.java
+++ src/main/java/ai/distil/integration/controller/dto/ScheduleConnectionSyncRequest.java
@@ -1,11 +1,9 @@
 package ai.distil.integration.controller.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ScheduleConnectionSyncRequest extends CommonConnectionRequest {
 
