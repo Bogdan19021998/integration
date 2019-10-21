bfe580418a49ffd7884b38b927d237ef763b8c57
Vitaliy Samofal <vsamofal93@gmail.com>
2019-10-21 11:16:31 +0300

    Added endpoint for checking destination settings by integration

--- src/main/java/ai/distil/integration/controller/DestinationIntegrationController.java
+++ src/main/java/ai/distil/integration/controller/DestinationIntegrationController.java
@@ -3,8 +3,7 @@ package ai.distil.integration.controller;
 import ai.distil.api.internal.model.dto.DestinationIntegrationSettingsDTO;
 import ai.distil.integration.controller.dto.BaseDestinationIntegrationRequest;
 import ai.distil.integration.controller.proxy.DestinationIntegrationProxy;
-import ai.distil.integration.service.ConnectionService;
-import ai.distil.integration.service.DataSyncService;
+import ai.distil.integration.service.DestinationIntegrationService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "Destination Integration Controller")
 public class DestinationIntegrationController implements DestinationIntegrationProxy {
 
-    private final ConnectionService connectionService;
-    private final DataSyncService dataSyncService;
+    private final DestinationIntegrationService destinationIntegrationService;
 
     @Override
     public ResponseEntity<DestinationIntegrationSettingsDTO> getDestinationIntegrationSettings(BaseDestinationIntegrationRequest request) {
-        return null;
+        return ResponseEntity.ok(destinationIntegrationService.retrieveDestinationIntegrationSettings(request));
     }
 }
